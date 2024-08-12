fc58d8ccb1521e2783f61bd616af8172566c56cb
Heartfelt Humorist <Heartfelt Humorist@Heartfelt-Humorist>
2024-08-12 23:06:07 +0200

    push

--- src/project_2/example2.java
+++ src/project_2/example2.java
@@ -1,5 +1,21 @@
 package project_2;
+package Package;
 
-public class example2 {
+import Package.class_1;
+
+public class class_1 {
+	int a;
+	public void display() // display is the method
+	{
+		System.out.println("Welcome to all of you");
+	}
+	public static void main(String[] args) {
+		class_1 sukerti=new class_1();
+        sukerti.display();   
+		System.out.println("I am the main method");
+		sukerti.display();
+		sukerti.a=34;
+		System.out.println(" Value of a"+ sukerti.a);
+	}
 
 }
